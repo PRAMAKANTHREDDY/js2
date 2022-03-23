495ebdc8b0bd036a67f800461ba15a4e9b2dfda4
PRAMAKANTHREDDY <74037226+PRAMAKANTHREDDY@users.noreply.github.com>
2022-03-23 14:07:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var s = "skill safari"
+console.log("length of string")
+console.log(s.length)
+console.log(s[4])
+console.log("Index of a")
+console.log(s.indexOf("a"))
+console.log("Capitalize a string")
+s = s.toUpperCase();
+console.log(s)
+console.log("Steve Rogers : Big man in a suit of armour. Take that off, what are you\nTony Stark : Genius, billionaire, playboy, philanthropist.\nSteve Rogers : I know guys with none of that worth ten of you. I've seen the footage. The only thing you really fight for is yourself. You're not the guy to make the sacrifice play, to lay down on a wire and let the other guy crawl over you.\nTony Stark : I think I would just cut the wire.\nSteve Rogers : Always a way out... You know, you may not be a threat, but you better stop pretending to be a hero.\nTony Stark : A hero? Like you? You're a lab rat, Rogers. Everything special about you came out of a bottle!\nSteve Rogers : Put on the suit. Let's go a few rounds.")
+console.log("BMI")
+var weight = 60;
+var height = 2.31;
+let bmi = weight / (height ** 2)
+console.log(bmi)
